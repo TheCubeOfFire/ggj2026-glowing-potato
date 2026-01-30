77294f0893c4790c9fc8835a8f01cb2fd0fc97dd
AntoMelon <>
2026-01-30 22:50:31 +0100

    Added player camera rotation

--- objects/player/main_player.gd
+++ objects/player/main_player.gd
@@ -10,24 +10,74 @@ extends CharacterBody3D
 @onready var gravity_vector: Vector3 = ProjectSettings.get_setting(&"physics/3d/default_gravity_vector")
 
 var movement_velocity: Vector3
+var rotation_target: Vector3
+
+var mouse_sensitivity: int = 700
+var gamepad_sensitivity: float = 0.075
+var mouse_captured: bool = true
+var input_mouse: Vector2 = Vector2.ZERO
 
 # ------- Overriden Engine Functions -------
-func _process(delta: float) -> void:
-    handle_input(delta)
-    handle_gravity(delta)
-
-    move_and_slide()
-    print(position)
+func _ready() -> void:
+    Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
     return
 
+func _physics_process(delta: float) -> void:
+    handle_movement(delta)
+    handle_gravity(delta)
+
+    handle_mouse_input(delta)
+    handle_rotation_input(delta)
+
+    move_and_slide()
+    return
+
+func _input(event: InputEvent) -> void:
+    if event is InputEventMouseMotion and mouse_captured:
+        var iemm := event as InputEventMouseMotion
+        input_mouse = iemm.screen_relative / mouse_sensitivity
+        handle_rotation(iemm.screen_relative.x, iemm.screen_relative.y, false)
+    return
 
 # ------- Other Functions -------
-func handle_input(_delta: float) -> void:
+func handle_movement(_delta: float) -> void:
     var input_vector: Vector2 = Input.get_vector(&"move_backward", &"move_forward", &"move_left", &"move_right")
     movement_velocity = Vector3(input_vector.x, 0, input_vector.y) * movement_speed
-    velocity = movement_velocity
+    velocity = transform.basis * movement_velocity
     return
 
 func handle_gravity(_delta: float) -> void:
     velocity += gravity_vector * gravity_force
+    return
+
+func handle_mouse_input(_delta: float) -> void:
+    # Mouse capture
+    if Input.is_action_just_pressed(&"mouse_capture"):
+        Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+        mouse_captured = true
+
+    if Input.is_action_just_pressed(&"mouse_capture_exit"):
+        Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+        mouse_captured = false
+        input_mouse = Vector2.ZERO
+    return
+
+func handle_rotation_input(_delta: float) -> void:
+    var invert_y_rotation: bool = false
+    var rotation_input := Input.get_vector(&"camera_left", &"camera_right",\
+                                            &"camera_down" if invert_y_rotation else &"camera_up",\
+                                            &"camera_up" if invert_y_rotation else &"camera_down")
+    if rotation_input:
+        handle_rotation(rotation_input.x, rotation_input.y, true, _delta)
+    return
+
+func handle_rotation(x_rot: float, y_rot: float, is_controller: bool, _delta: float = 0.0) -> void:
+    if is_controller:
+        rotation_target += Vector3(-y_rot, -x_rot, 0).limit_length(1.0) * gamepad_sensitivity
+    else:
+        rotation_target += (Vector3(-y_rot, -x_rot, 0) / mouse_sensitivity)
+
+    rotation_target.x = clamp(rotation_target.x, deg_to_rad(-90), deg_to_rad(90))
+    camera.rotation.x = rotation_target.x
+    rotation.y = rotation_target.y
     return
