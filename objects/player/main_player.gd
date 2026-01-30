df367b5115390fa4d85e74f282d3ec517e110667
AntoMelon <>
2026-01-30 21:53:37 +0100

    Added player gravity basis

--- objects/player/main_player.gd
+++ objects/player/main_player.gd
@@ -6,14 +6,18 @@ extends CharacterBody3D
 
 # ------- Internal vars -------
 @onready var camera: Camera3D = $Camera3D
+@onready var gravity_force: float = ProjectSettings.get_setting(&"physics/3d/default_gravity")
+@onready var gravity_vector: Vector3 = ProjectSettings.get_setting(&"physics/3d/default_gravity_vector")
 
 var movement_velocity: Vector3
 
 # ------- Overriden Engine Functions -------
 func _process(delta: float) -> void:
     handle_input(delta)
+    handle_gravity(delta)
 
     move_and_slide()
+    print(position)
     return
 
 
@@ -22,4 +26,8 @@ func handle_input(_delta: float) -> void:
     var input_vector: Vector2 = Input.get_vector(&"move_backward", &"move_forward", &"move_left", &"move_right")
     movement_velocity = Vector3(input_vector.x, 0, input_vector.y) * movement_speed
     velocity = movement_velocity
+    return
+
+func handle_gravity(_delta: float) -> void:
+    velocity += gravity_vector * gravity_force
     return
