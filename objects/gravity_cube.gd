312a1745098bb510ef6fc5fc75d2cec2828ec4f4
AntoMelon <>
2026-01-31 10:25:18 +0100

    Made GravityCube an AnimatableBody3D

--- objects/gravity_cube.gd
+++ objects/gravity_cube.gd
@@ -1,27 +1,31 @@
 class_name GravityCube
-extends RigidBody3D
+extends AnimatableBody3D
 
 # ------- Internal vars -------
 @onready var default_gravity_force: float = ProjectSettings.get_setting(&"physics/3d/default_gravity")
-@onready var default_gravity_vector: Vector3 = ProjectSettings.get_setting(&"physics/3d/default_gravity_vector")
 
 var is_gravity_overrriden: bool = false
-var gravity_override: Vector3 = Vector3.ZERO
+var gravity: Vector3 = Vector3.ZERO
 
 # ------- Overriden Engine Functions -------
-func _physics_process(_delta: float) -> void:
-    if is_gravity_overrriden:
-        apply_central_force(default_gravity_force * default_gravity_vector * -1)
-        apply_central_force(default_gravity_force * gravity_override)
+func _physics_process(delta: float) -> void:
+    #if Input.is_action_just_pressed(&"debug_test"):
+        #if is_gravity_overrriden:
+            #clear_gravity_override()
+        #else:
+            #override_gravity(Vector3(0,1,0))
+
+    var movement: Vector3 = default_gravity_force * gravity * delta
+    move_and_collide(movement)
     return
 
 # ------- Functions -------
 func override_gravity(new_gravity: Vector3) -> void:
     is_gravity_overrriden = true
-    gravity_override = new_gravity.normalized()
+    gravity = new_gravity.normalized()
     return
 
 func clear_gravity_override() -> void:
     is_gravity_overrriden = false
-    gravity_override = Vector3.ZERO
+    gravity = Vector3.ZERO
     return
