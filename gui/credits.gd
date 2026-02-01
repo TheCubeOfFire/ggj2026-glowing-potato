b8791b985afd3f999521b3cce1ecc2ab21f731c0
AntoMelon <>
2026-02-01 23:25:20 +0100

    Improved menu controller navigation

--- gui/credits.gd
+++ gui/credits.gd
@@ -6,3 +6,9 @@ extends QuickGorillaCredits
 func _ready() -> void:
     super._ready()
     credits_list.add_text(get_full_engine_credits())
+    back_button.grab_focus.call_deferred()
+
+func _input(event: InputEvent) -> void:
+    if event.is_action_released(&"ui_cancel"):
+        _on_back_button_pressed()
+    return
