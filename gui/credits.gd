cb1c5614a3bc470db8156b7c13c22c3133233194
Fabien Matusalem <matusalemf@aol.com>
2026-02-02 17:54:14 +0100

    Minor UI fixes
    Fix credits navigability with gamepad
    Improve credits readability with a back panel
    Credits' rich text is now focused by default, helping navigation in this
    menu
    Increase difference between focused and unfocused buttons
    Change scrollbar theme to better fit the rest of the game
    Add a default custom theme for the whole game UI by merging the existing
    themes into one

--- gui/credits.gd
+++ gui/credits.gd
@@ -1,14 +1,11 @@
 class_name Credits
 extends QuickGorillaCredits
 
-@onready var credits_list: RichTextLabel = $VBoxContainer/credits_list
+
+@onready var _credits_list := %CreditsList as RichTextLabel
+
 
 func _ready() -> void:
     super._ready()
-    credits_list.add_text(get_full_engine_credits())
-    back_button.grab_focus.call_deferred()
-
-func _input(event: InputEvent) -> void:
-    if event.is_action_released(&"ui_cancel"):
-        _on_back_button_pressed()
-    return
+    _credits_list.add_text(get_full_engine_credits())
+    _credits_list.grab_focus.call_deferred()
